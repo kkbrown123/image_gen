ac68be4212d60ff645c4dce44d1ebbc524409a8c
kkbrown123 <53658765+kkbrown123@users.noreply.github.com>
2023-06-03 23:44:36 -0500

    Update image_gen.py

--- image_gen.py
+++ image_gen.py
@@ -2,13 +2,19 @@ import os
 import openai
 import requests
 
-openai.api_key = "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"
-openai.organization = "org-zAqoMaaF2XLgYGbUhS12hgJr"
-openai.Model.list()
+# https://platform.openai.com/docs/guides/images/usage
+# https://platform.openai.com/docs/api-reference/images/create-edit
 
-folder_name = 'kev_ima'
+
+# Unique api_key for a user goes here and organization
+openai.api_key = " "
+openai.organization = " "
+openai.Model.list()
+# Folder name to store image
+folder_name = ''
 if not os.path.isdir(folder_name):
     os.makedirs(folder_name)
+
 
 def image_d(url, folder_name, num):
     response = requests.get(url)
@@ -17,16 +23,12 @@ def image_d(url, folder_name, num):
             file.write(response.content)
 
 
-for i in range(50,61):
+for i in range(50, 61):
     response = openai.Image.create(
-    prompt=" logo for ceres robotics limited creates drone for  argiculture",
-    n=1,
-    size="1024x1024"
+        # Add prompt here
+        prompt=" ",
+        n=1,
+        size="1024x1024"
     )
     image_url = response['data'][0]['url']
-    image_d(image_url,folder_name,i)
-    
-
-
-
-
+    image_d(image_url, folder_name, i)
